686fc88eab4e01b64305d011889cdbcaa76591c3
100whitebirds <avenlardo@gmail.com>
2022-09-04 18:05:55 +0300

    day 3

---
+++ src/longestSubstring.js
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+const lengthOfLongestSubstring = (s) => {
+  let begin = 0;
+  let maxLen = 0;
+  const map = {};
+
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]] !== undefined && map[s[i]] >= begin) {
+      begin = i;
+    }
+    map[s[i]] = i;
+    maxLen = Math.max(maxLen, i - begin + 1);
+  }
+  return maxLen;
+};
+
+const s = 'abcabcbb';
+
+console.log(lengthOfLongestSubstring(s));
