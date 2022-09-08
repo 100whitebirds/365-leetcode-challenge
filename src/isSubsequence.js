b27967023b15a12e67f79eaaf249e5ad53e42fa6
100whitebirds <avenlardo@gmail.com>
2022-09-08 12:03:54 +0300

    day 6(slipped)

---
+++ src/isSubsequence.js
@@ -0,0 +1,16 @@
+const isSubsequence = (s, t) => {
+  const a = s.split('');
+  let b = 0;
+
+  for (let i = 0; i < t.length; i++) {
+    if (a[b] === t[i]) {
+      b++;
+    }
+  }
+  return b === s.length;
+};
+
+const s = 'b';
+const t = 'c';
+
+console.log(isSubsequence(s, t));
